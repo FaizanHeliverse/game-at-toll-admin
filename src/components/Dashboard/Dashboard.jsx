38d2f6da1fd24d77e2cc79b3ef2623d016e21df9
FaizanHeliverse <faizan@heliverse.com>
2021-12-11 05:02:39 +0530

    fix

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,58 +1,65 @@
-    import React from 'react';
-    import './Dashboard.css';
-    import GameTable from '../GameTable/GameTable';
-    import Graphs from '../Graphs/Graphs'
+import React from "react";
+import "./Dashboard.css";
+import GameTable from "../GameTable/GameTable";
+import Graphs from "../Graphs/Graphs";
+import BarGraph from "../Graphs/BarGraph"
+import { fetchData } from "../../middleware/RequestHandler";
 
-    function Dashboard() {
-        let today = new Date().toISOString().slice(0, 10)
+function Dashboard() {
+  const [gameMostPlayed, setGameMostPlayed] = React.useState([]);
 
-console.log(today)
-        return (
-            <div className="dashboard_container">
-                <h1>Dashboard</h1>
-                <div className="match">
-                <h2>Today Match</h2>
-                <h2>{today}</h2>
+  React.useEffect(async () => {
+    const response = await fetchData("/getMostPlayedGames", { method: "GET" });
+    console.log(response);
 
-                </div>
-                <div className="box">
-                  
-                <div className="sub_box">
-                    
-                      <div>Total Matches</div>
-                      <h2>23</h2>
-                </div>
-                <div className="sub_box">
-                 
-                  <div>Game Name</div>
-                  <h2>PupG</h2>
-                </div>
+ 
+    setGameMostPlayed(response);
+    
+  }, []);
 
-              <div className="sub_box">
-                 
-               <div>Match Start</div>
-               <h2>2:40 P.M</h2>
-              </div>
-              <div className="sub_box">
-             
-               <div>Match fees Today</div>
-               <h2>$10</h2>
-           </div>
-           </div>
+  let today = new Date().toISOString().slice(0, 10);
 
-           <div className="today_match_user_table">
-               <h2>Today Active Match User</h2>
-               <GameTable data={[]}/>
-            
-                  </div>
-                  <div className="graph">
-                      <h2>Last 7 Days Matches`  </h2>
-                  <Graphs/>
-                  </div>
-                 
+  console.log(today);
+  return (
+    <div className="dashboard_container">
+      <h1>Dashboard</h1>
+      <div className="match">
+        <h2>Today Match</h2>
+        <h2>{today}</h2>
+      </div>
+      <div className="box">
+        <div className="sub_box">
+          <div>Total Matches</div>
+          <h2>23</h2>
+        </div>
+        <div className="sub_box">
+          <div>Game Name</div>
+          <h2>PupG</h2>
+        </div>
+
+        <div className="sub_box">
+          <div>Match Start</div>
+          <h2>2:40 P.M</h2>
+        </div>
+        <div className="sub_box">
+          <div>Match fees Today</div>
+          <h2>$10</h2>
+        </div>
+      </div>
+      <div className="graph">
           
-            </div>
-        )
-    }
+        <BarGraph/>
+        <Graphs gameData={gameMostPlayed} />
+        
+      </div>
 
-    export default Dashboard
+      <div className="today_match_user_table">
+        <h2>Today Active Match User</h2>
+        <GameTable data={[]} />
+      </div>
+      
+    </div>
+  );
+}
+
+export default Dashboard;
